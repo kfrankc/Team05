6efe9d9f6b67f5296d551c0165c922e9c42f7eb1
Brandon <bhaffen97@gmail.com>
2017-02-04 17:02:44 -0800

    Converted some tabs to spaces to fix spacing

--- server_config_parser_test.cc
+++ server_config_parser_test.cc
@@ -6,21 +6,21 @@ class PortParseTest : public ::testing::Test {
 protected:
 
     bool getPortNumber(std::string config_contents) {
-       	int fd;
-		char name[] = "/tmp/fileXXXXXX";
-		
-		// Creates a temporary file using name, replacing the last 6 X's 
-		fd = mkstemp(name);
+        int fd;
+        char name[] = "/tmp/fileXXXXXX";
 
-		// Write the config_contents into the temp file
-		write(fd, config_contents.c_str(), config_contents.size());
-    	
-    	port = port_number(name);
-    	
-    	// Delete the temporary file
-    	remove(name);
-    	
-		return (port != -1);
+        // Creates a temporary file using name, replacing the last 6 X's 
+        fd = mkstemp(name);
+
+        // Write the config_contents into the temp file
+        write(fd, config_contents.c_str(), config_contents.size());
+
+        port = port_number(name);
+
+        // Delete the temporary file
+        remove(name);
+
+        return (port != -1);
     }
 
     int port;
@@ -28,12 +28,12 @@ protected:
 
 
 TEST_F(PortParseTest, ValidPortConfig) {
-	ASSERT_TRUE(getPortNumber("port 80800;"));
-	EXPECT_EQ(80800, port);
+    ASSERT_TRUE(getPortNumber("port 80800;"));
+    EXPECT_EQ(80800, port);
 }
 
 
 TEST_F(PortParseTest, InvalidPortConfig) {
-	EXPECT_FALSE(getPortNumber("Port 80800;"));
+    EXPECT_FALSE(getPortNumber("Port 80800;"));
 }
 
