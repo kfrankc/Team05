acf7c405cc428e8af0b4754cf96a23857382709f
Alexander Fong <alpfong@gmail.com>
2017-03-03 18:34:01 -0800

    Modify proxy handler to update Host header

--- reverse_proxy_handler.cc
+++ reverse_proxy_handler.cc
@@ -144,25 +144,6 @@ std::string ReverseProxyHandler::sendRequestToOrigin(Request request, std::strin
     boost::asio::io_service io_service;
     tcp::socket socket(io_service);
     tcp::resolver resolver(io_service);
-    // Avoid "host not found" error
-    // See: https://stackoverflow.com/questions/12542460/boost-asio-host-not-found-authorative
-    tcp::resolver::query query(remote_host,
-                               remote_port,
-                               boost::asio::ip::resolver_query_base::numeric_service);
-
-    // Use resolver to handle DNS lookup if query is not an IP address
-    boost::system::error_code ec;
-
-    // A hostname could resolve to multiple endpoints to try;
-    // connect() will try all of them
-    // See: http://www.boost.org/doc/libs/1_62_0/boost/asio/connect.hpp
-    boost::asio::connect(socket, resolver.resolve(query), ec);
-    if (ec == boost::asio::error::not_found) {
-        printf("Reverse proxy connection attempt to remote host failed. Check hostname and port number.\n");
-        return "RequestHandler::Error";
-    }
-
-    std::cerr << "Got past connecting to remote_host!" << std::endl;
 
     // Construct new request to send to remote host
     // Example: Modify request from: /reverse_proxy/static/file1.txt
@@ -172,39 +153,78 @@ std::string ReverseProxyHandler::sendRequestToOrigin(Request request, std::strin
     if (remote_uri.empty()) {
       remote_uri = "/";
     }
-
     request.SetUri(remote_uri);
-    std::string remote_request = request.ToString();
 
-    std::cerr << "Sending remote_request: " << remote_request << std::endl;
+    // Use resolver to handle DNS lookup if query is not an IP address
+    boost::system::error_code ec;
+    std::string new_response = "";
+    bool got_302 = false;
+    do {
+      // Avoid "host not found" error
+      // See: https://stackoverflow.com/questions/12542460/boost-asio-host-not-found-authorative
+      tcp::resolver::query query(remote_host,
+                                 remote_port,
+                                 boost::asio::ip::resolver_query_base::numeric_service);
 
-    // TODO: for some reason the server fails to transfer an image through the reverse_proxy.
-    // Right now, it will send like 4 bytes then just sit there. Also, requests to
-    // /reverse_proxy/echo and /reverse_proxy/status through curl are correctly recieved and
-    // when diff'd they show nothing. However, firefox will try to download the file instead
-    // of rendering text for every handler.
-    boost::asio::write(socket, boost::asio::buffer(remote_request, remote_request.size()));
+      // A hostname could resolve to multiple endpoints to try;
+      // connect() will try all of them
+      // See: http://www.boost.org/doc/libs/1_62_0/boost/asio/connect.hpp
+      boost::asio::connect(socket, resolver.resolve(query), ec);
+      if (ec == boost::asio::error::not_found) {
+          printf("Reverse proxy connection attempt to remote host failed. Check hostname and port number.\n");
+          return "RequestHandler::Error";
+      }
 
-    // Based on: https://github.com/UCLA-CS130/Mr.-Robot-et-al./blob/c9b064c68cd4bc1ae6b5c012db59eae9cb8b946d/lightning_server.cc#L26
-    const int MAX_BUFFER_LENGTH = 8192;
-    char response_buffer[MAX_BUFFER_LENGTH];
-    memset(response_buffer, 0, MAX_BUFFER_LENGTH);
+      std::cerr << "Got past connecting to remote_host!" << std::endl;
 
-    // TODO: need to loop and keep reading if we recieve a large file, `read_some` blocks until
-    // it reads a non-zero number of bytes, but have to somehow check against its return value
-    int bytes_read = socket.read_some(boost::asio::buffer(response_buffer), ec);
-    std::cout << "bytes_read: " << bytes_read << std::endl;
+      std::string remote_request = request.raw_request();
+      size_t host_pos = remote_request.find("Host");
+      size_t host_end = remote_request.find("\r\n", host_pos);
+      size_t host_len = host_end - host_pos;
+      remote_request.replace(host_pos, host_len, "Host: " + remote_host);
+      std::cerr << "---------Sending remote_request-----------\n"
+                << remote_request
+                << "----------End of remote_request----------\n";
+      socket.send(boost::asio::buffer(remote_request));
 
+      const int MAX_BUFFER_LENGTH = 1024;
+      size_t bytes_received = 0;
+      do {
+        char response_buffer[MAX_BUFFER_LENGTH];
+        bytes_received = socket.receive(boost::asio::buffer(response_buffer), {}, ec);
+        if (!ec) {
+          new_response.append(response_buffer, response_buffer + bytes_received);
+        }
+        std::cerr << "bytes_received: " << bytes_received << "    ec: " << ec << std::endl;
+      } while (!ec);
+
+      if (new_response.find("HTTP/1.1 302 Found") != std::string::npos) {
+        got_302 = true;
+        size_t location = new_response.find("Location");
+        size_t www = new_response.find("www", location);
+        size_t slash = new_response.find("/", www);
+        remote_host = new_response.substr(www, slash - www);
+        new_response = "";
+        std::cout << "Redirected: setting remote_host to " << remote_host << std::endl;
+      }
+      else {
+        got_302 = false;
+      }
+      std::cout << "Got_302? " << got_302 << std::endl;
+    } while (got_302);
+
+    // Checking ec set when reading response from remote host
     switch (ec.value()) {
+      case boost::asio::error::eof:
       case boost::system::errc::success:
-        std::cout << "remote_host's response: \n" << response_buffer << std::endl;
+        std::cerr << "remote_host's response: \n" << new_response << std::endl;
         break;
       default:
-        std::cout << "Error reading from remote_host, error code: " << ec << std::endl;
+        std::cerr << "Error reading from remote_host, error code: " << ec << std::endl;
         return "RequestHandler::Error";
     }
 
-    return std::string(response_buffer);
+    return new_response;
 }
 
 // Helper for editing the path of relative URIs
@@ -238,26 +258,6 @@ RequestHandler::Status ReverseProxyHandler::HandleRequest(const Request& request
     std::string return_response_code = getRemoteResponseCode(response_buffer_string);
     std::cerr << "return_response_code: " << return_response_code << std::endl;
 
-    // To avoid infinite loops, 302 redirects are followed at most 5 times
-    int num_tries = 0;
-    const int MAX_TRIES = 5;
-    while (return_response_code == "302") {
-        if (num_tries >= MAX_TRIES) {
-            return_response_code = "404";
-            printf("Max number of 302 Found redirects followed. Potential redirect loop.\n");
-            break;
-        }
-
-        std::string new_remote_uri = getLocationHeaderValue(response_buffer_string);
-        std::cout << "\nTrying to send another request with remote_url: " << new_remote_uri << std::endl << std::endl;
-        // TODO: should this be setting the Host: or Location: header instead of uri?
-        // for example, when redirected from ucla.edu, new_remote_uri = http://www.ucla.edu
-        response_buffer_string = sendRequestToOrigin(request, new_remote_uri);
-        return_response_code = getRemoteResponseCode(response_buffer_string);
-
-        num_tries++;
-    }
-
     if (return_response_code == "200") {
         response->SetStatus(Response::ok);
     } else if (return_response_code == "404") {
@@ -266,36 +266,7 @@ RequestHandler::Status ReverseProxyHandler::HandleRequest(const Request& request
         response->SetStatus(Response::internal_server_error);
     }
 
-    // Construct response headers by including every line of
-    // the remote_host's response after the headers
-    // NOTE: find() matches an entire sequence; find_first_of() matches
-    // the first of any character specified in the search pattern
-    int end_headers = response_buffer_string.find("\r\n\r\n");
-
-    std::cerr << "end_headers index: " << end_headers << std::endl;
-
-    // Copying over headers from the remote response
-    int header_start = response_buffer_string.find("\r\n") + 2;
-    int header_end = response_buffer_string.find("\r\n", header_start);
-    while (header_end <= end_headers) {
-      // Find ':' separating header key and header value, then add two to get to the start
-      // of the header value.
-      int colon_pos = response_buffer_string.find_first_of(':', header_start);
-      int header_type_len = colon_pos - header_start;
-      int header_value_len = header_end - colon_pos - 1;
-      std::string header_type = response_buffer_string.substr(header_start, header_type_len);
-      std::string header_value = response_buffer_string.substr(colon_pos+2, header_value_len);
-      response->AddHeader(header_type, header_value);
-
-      // After processing current header, move position past the previously found "\r\n"
-      header_start = header_end + 2;
-      header_end = response_buffer_string.find("\r\n", header_start);
-    }
-
-    // Erase headers from the response after adding them to the Response object
-    // the +4 is to skip the double carriage return
-    response_buffer_string.erase(0, end_headers + 4);
-    response->SetBody(response_buffer_string);
+    response->SetFullResponse(response_buffer_string);
 
     return RequestHandler::OK;
 }
